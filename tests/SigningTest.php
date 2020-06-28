09881e670fd23a7f8c758cce84b724a8d6ab6863
Samuel Elliott <samuel@fancy.org.uk>
2020-06-28 13:29:08 +0100

    Fix tests

--- tests/SigningTest.php
+++ tests/SigningTest.php
@@ -16,17 +16,17 @@ final class SigningTest extends TestCase
         'could, to where it bent in the undergrowth.';
 
     // Signed with a hardcoded keypair
-    const SIGNED_ARMORED = 'BEGIN SALTPACK MESSAGE. kYM5h1pg6qz9UMn j6G9T0lmMjkYOsZ Kn4Acw58u39dn3B kmdpuvqpO3t2QdM ' .
-        'CnBX5wO3tOv7bWQ C0ZjZmZwkVc0000 000000000000000 00000000xTSC6Pg rfGb2LrM9GBjenP w3DX3Ds8TxgDfKJ ' .
-        'opD87cg3NkGYdfr YcvCu8lglIAFuU2 MKITUw0xscRdmtJ M1v1jgyZjEH5Vem dFbYdN5s9bkJE42 hEUFOKNdklXPbnW ' .
-        'N7ftTO2L4pqwnl4 5s9QAyu8GLIeOCh nGSD3XNOUPToxH3 Pelc42Xz5oOVMI3 c2ScE8gRgTQ9UAM VXnut3siWpTSEqQ ' .
-        'VHhLbdfUDCmKdIQ QKXoeeJBRilOtbN gckxla11c4v6Qzw tlPxv7p6y2e8N6i Paehy2YQ8IFggor 7CMMLWrnzrU40vL ' .
-        'vCFTlwsK8G3Xeok f8UrxgICsgJapXL xe. END SALTPACK MESSAGE.';
+    const SIGNED_ARMORED = 'BEGIN SALTPACK SIGNED MESSAGE. kYM5h1pg6qz9UMn j6G9T0lmMjkYOsZ Kn4Acw58u39dn3B ' .
+        'kmdpuvqpO3t2QdM CnBX5wO3tOv7bWQ C0ZjZmZwkVc0000 000000000000000 00000000xTSC6Wm fygB9EKusQb8Znq ' .
+        'EWXDdTd0JL4mTLM 4WpyJxXYaA6vhBp CYfzQJ5B3Ob9zmK w7S214B22XdNv0R uq1AeFybkIHsvtv rVrLDN5s9bkJE42 ' .
+        'hEUFOKNdklXPbnW N7ftTO2L4pqwnl4 5s9QAyu8GLIeOCh nGSD3XNOUPToxH3 Pelc42Xz5oOVMI3 c2ScE8gRgTQ9UAM ' .
+        'VXnut3siWpTSEqQ VHhLbdfUDCmKdIQ QKXoeeJBRilOtbN gckxla11c4v6Qzw tlPxv7p6y2e8N6i Paehy2YQ8IFggor ' .
+        '7CMMLWrnzrU40vL vCFTlwsK8G3Xeok f8UrxgICsgJapXL xe. END SALTPACK SIGNED MESSAGE.';
     const SIGNED_HEX = 'c45295a873616c747061636b92020001c4203b6a27bcceb6a42d62a3a8d02a6f' .
         '0d73653215771de243a63ac048a18b59da29d920000000000000000000000000' .
-        '000000000000000000000000000000000000000093c3c4409437e3d24a86c389' .
-        '44458705d8a9410b37eb65c163c2070c0907f0251379a4a2f9083e7baa00be6f' .
-        'b4558d56b3fc861832fb98cdb46ec039d2f46082ebf6fa08d9b654776f20726f' .
+        '000000000000000000000000000000000000000093c3c440e6d7eac8b8279c56' .
+        '702b91396777698a1adf14d0f2c63d4fccb735885255938369dd62b65d4a80ef' .
+        '2e9e72da00c223d3bd1a6499de6703f040f1a483e65b4105d9b654776f20726f' .
         '61647320646976657267656420696e20612079656c6c6f7720776f6f642c2061' .
         '6e6420736f727279204920636f756c64206e6f742074726176656c20626f7468' .
         '0a616e64206265206f6e652074726176656c6c65722c206c6f6e672049207374' .
@@ -34,14 +34,15 @@ final class SigningTest extends TestCase
         '617320490a636f756c642c20746f2077686572652069742062656e7420696e20' .
         '74686520756e64657267726f7774682e';
 
-    const DETACHED_SIGNATURE_ARMORED = 'BEGIN SALTPACK MESSAGE. kYM5h1pg6qz9UMn j6G9T0tZQlxoky3 0YoKQ4s21IrFv3B ' .
-        'kmdpuvqpO3t2QdM CnBX5wO3tOv7bWQ C0ZjZmZwkVc0000 000000000000000 00000001GzTLjk5 Ttisc5wVwXdGuuL ' .
-        'gp2AM0dwVcjFToE PddQafkHFkvFole iXj55me8KV5302a IORauTnukaCk2eA ReNSnzn. END SALTPACK MESSAGE.';
+    const DETACHED_SIGNATURE_ARMORED = 'BEGIN SALTPACK DETACHED SIGNATURE. kYM5h1pg6qz9UMn j6G9T0tZQlxoky3 ' .
+        '0YoKQ4s21IrFv3B kmdpuvqpO3t2QdM CnBX5wO3tOv7bWQ C0ZjZmZwkVc0000 000000000000000 00000001GzUJvbQ ' .
+        'ok7ysE5tdLoOMQo rzUxGAGrCmBjvpK AUk6aScpEO4JJHm OT2neYw5p4sOxzi HoqGTDUfkXUtVDf hL0O6CZ. ' .
+        'END SALTPACK DETACHED SIGNATURE.';
     const DETACHED_SIGNATURE_HEX = 'c45295a873616c747061636b92020002c4203b6a27bcceb6a42d62a3a8d02a6f' .
         '0d73653215771de243a63ac048a18b59da29d920000000000000000000000000' .
-        '0000000000000000000000000000000000000000c440bf27a9aa7201dfe9f39c' .
-        '8dd5becdeeeb430fd79d13dd0da80ffab41f4444dee626d53abae94290026664' .
-        'd452fdc00d9bd1b96a4fc85e21fb08cfed90e16ff40b';
+        '0000000000000000000000000000000000000000c440e846e567f7e1e5c1f2bd' .
+        '5bfc7f5ccc7efa606dec633de96e4530017a8da06ce0d0c409da5e3d1f05880e' .
+        '946ecbb0e643ea6490a6c4ac8160927689354bef2703';
 
     /** @var string $keypair */
     private $keypair;
@@ -57,7 +58,7 @@ final class SigningTest extends TestCase
     public function testSign(): void
     {
         $signed = Signing::sign(self::INPUT_STRING, $this->keypair);
-        $armored = Armor::armor($signed);
+        $armored = Armor::armor($signed, ['message_type' => 'SIGNED MESSAGE']);
 
         $this->assertEquals(self::SIGNED_ARMORED, $armored);
         $this->assertEquals(self::SIGNED_HEX, bin2hex($signed));
@@ -106,7 +107,7 @@ final class SigningTest extends TestCase
     public function testSignDetached(): void
     {
         $signed = Signing::signDetached(self::INPUT_STRING, $this->keypair);
-        $armored = Armor::armor($signed);
+        $armored = Armor::armor($signed, ['message_type' => 'DETACHED SIGNATURE']);
 
         $this->assertEquals(self::DETACHED_SIGNATURE_ARMORED, $armored);
         $this->assertEquals(self::DETACHED_SIGNATURE_HEX, bin2hex($signed));
